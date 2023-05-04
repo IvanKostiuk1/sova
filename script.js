4ca4cfe35bc02d53848dc42e7ae4c4e19356341b
IvanKostiuk1 <ivan.kostiuk@student.tabyenskilda.se>
2023-05-04 12:49:29 +0200

    v.2

---
+++ script.js
@@ -0,0 +1,26 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropdown')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  function vändPilen(idElement) {
+    var element = document.getElementById('pil' + idElement);
+    if (idElement === 1 || idElement === 2) {
+        if (element.innerHTML === '▼') element.innerHTML = '▲';
+        else {
+            element.innerHTML = '▼';
+        }
+    }
+}
