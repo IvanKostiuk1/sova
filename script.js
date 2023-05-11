d93926d2584d3a9fc83b945a175e87682167905d
IvanKostiuk1 <ivan.kostiuk@student.tabyenskilda.se>
2023-05-11 10:49:07 +0200

    v.4

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function myFunction() {
   function vändPilen(idElement) {
     var element = document.getElementById('pil' + idElement);
     if (idElement === 1 || idElement === 2) {
-        if (element.innerHTML === '▼') element.innerHTML = '▲';
+        if (element.innerHTML === '▼') element.innerHTML = '►';
         else {
             element.innerHTML = '▼';
         }
